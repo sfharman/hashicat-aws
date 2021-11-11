17149cb18f1cd5ad42d1e05e7b25b724b84a0179
sfharman <sfharman@gmail.com>
2021-11-11 16:01:09 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws 2.2.0"
+  source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket"
   bucket_prefix = "steveharman"
